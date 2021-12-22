b93eeb316688b61010da4f2278b0462a745800a5
Yamano <ituyama01@outlook.jp>
2021-12-22 21:39:09 +0900

    Fix:アイコンの修正

--- js/index.js
+++ js/index.js
@@ -13,7 +13,7 @@ Vue.component('NavHeader', {
     <router-link class="navbar-brand" to="/">YamanoItsuki</router-link>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
         aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+        <span class="bi-signpost-2"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
